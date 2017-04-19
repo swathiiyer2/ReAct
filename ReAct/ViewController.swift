6a2de5ef34b932b92f1d5d7914b1510f4aeca5ad
Swathi Iyer <swathii@stanford.edu>
2017-04-19 13:07:42 -0700

    Initial Commit

---
+++ ReAct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReAct
+//
+//  Created by Swathi Iyer on 4/19/17.
+//  Copyright © 2017 Swathi Iyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
